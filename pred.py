a01a68eb6c70581abc6556c1c1e58899267ae62d
Meet Koriya <meetkoriya13@gmail.com>
2021-10-20 16:10:54 +0530

    fix bugs

--- pred.py
+++ pred.py
@@ -2,15 +2,17 @@ import streamlit as st
 import pickle
 import numpy as np
 
-def load():
+
+def load_model():
     with open('data/saved_steps.pkl', 'rb') as file:
         data = pickle.load(file)
         return data
-data = load()
+
+data = load_model()
 
 regressor = data["model"]
 le_country = data["le_country"]
-lableEncoder_ed = data["le_education"]
+le_education = data["le_education"]
 
 def show_pred():
     st.title("Salary Prediction of Software developer")
@@ -18,19 +20,43 @@ def show_pred():
     st.write("""### Kindly add some information to predict the salary""")
 
     countries = (
-        "United States of America","India","Germany","United Kingdom","Canada","France","Brazil","Spain","Netherlands","Australia","Poland","Italy","Russia","Sweden","Turkey","Switzerland","Israel ",)
-    education = (
-        "Less than Bachelors", "Bachelor's Degree", "Master's Degree", "Post Grad",
-    )
-    country = st.selectbox("Country", countries)
-    edu = st.selectbox("Education Level", education)
+        "United States of America",
+        "India",
+        "Germany",
+        "United Kingdom of Great Britain and Northern Ireland",
+        "Canada",
+        "France",
+        "Brazil",
+        "Spain",
+        "Netherlands",
+        "Australia",
+        "Poland",
+        "Italy",
+        "Russian Federation",
+        "Sweden",
+        "Turkey",
+        "Switzerland",
+        "Israel ",
+        "Norway"
+        )
 
-    experi = st.slider("Years of Experience", 0, 50, 3)
+    education = (
+        "Less than a Bachelors",
+        "Bachelor’s degree",
+        "Master’s degree",
+        "Post grad",
+    )
+
+    country = st.selectbox("Country", countries)
+    education = st.selectbox("Education Level", education)
+
+    expericence = st.slider("Years of Experience", 0, 50, 3)
     ok = st.button("Calculate salary")
+
     if ok:
-        X = np.array([[country, edu, experi ]])
+        X = np.array([[country, education, expericence ]])
         X[:, 0] = le_country.transform(X[:,0])
-        X[:, 1] = lableEncoder_ed.transform(X[:,1])
+        X[:, 1] = le_education.transform(X[:,1])
         X = X.astype(float)
 
         salary = regressor.predict(X)
